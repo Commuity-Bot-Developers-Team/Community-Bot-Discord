b2fe3770b123b697800ba1f02110730ba561e068
sairam4123 <opencity@info.github.com>
2020-06-27 10:08:56 +0530

    Update 0.0.12.1 By Sairam4123
    Fixed Starboard (Untested)

--- Bot/cogs/Starboard.py
+++ Bot/cogs/Starboard.py
@@ -76,47 +76,107 @@ class Starboard(commands.Cog):
                 """, payload.guild_id)
                 star_board_channel = self.bot.get_channel(star_board_channel_id)
                 star_channel = self.bot.get_channel(payload.channel_id)
-                if star_channel:
+                if star_board_channel:
                     star_message_row = await self.bot.pg_conn.fetchrow("""
-                    SELECT stars, star_message_id 
-                    FROM starboard_data 
+                    SELECT * FROM starboard_data 
                     WHERE root_message_id = $1
-                    """, payload.guild_id)
-                    if not star_message_row:
-                        stars = star_message_row[3]
-                        stars += 1
-                        if stars == pin:
-                            message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
-                            embed = discord.Embed(colour=get_colour(), description=message.content)
-                            embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
-                            embed.timestamp = datetime.datetime.utcnow()
-                            embed.add_field(name="Original", value=f"[Jump Url]({message.jump_url})", inline=False)
-                            msg = await star_board_channel.send(content=f"**{stars}** :star: {star_channel.mention} ID : {payload.message_id}", embed=embed)
+                    """, payload.message_id)
+                    print(star_message_row)
+                    stars = star_message_row[3] if star_message_row else 0
+                    stars += 1
+                    print(stars)
+                    if stars == pin:
+                        message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+                        embed = discord.Embed(colour=get_colour(), description=message.content)
+                        embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+                        embed.timestamp = datetime.datetime.utcnow()
+                        embed.add_field(name="Original", value=f"[Jump Url]({message.jump_url})", inline=False)
+                        msg = await star_board_channel.send(content=f"**{stars}** :star: {star_channel.mention} ID : {payload.message_id}", embed=embed)
+                        await self.bot.pg_conn.execute("""
+                        INSERT INTO starboard_data 
+                        VALUES ($1, $2, $3, $4)
+                        """, payload.guild_id, payload.message_id, msg.id, stars)
+                    elif stars > pin:
+                        try:
+                            start_board_message = await star_board_channel.fetch_message(star_message_row[2])
+                            await start_board_message.edit(content=f"**{stars}** :star: {star_channel.mention} ID : {payload.message_id}")
                             await self.bot.pg_conn.execute("""
+                            UPDATE starboard_data 
+                            SET stars = $1 
+                            WHERE star_message_id = $2 AND root_message_id = $3
+                            """, stars, start_board_message.id, payload.message_id)
+                        except (discord.Forbidden, discord.NotFound):
+                            pass
+                    elif stars < pin:
+                        try:
+                            start_board_message = await star_board_channel.fetch_message(star_message_row[2])
+                            await start_board_message.edit(content=f"**{stars}** :star: {star_channel.mention} ID : {payload.message_id}")
+                            await self.bot.pg_conn.execute("""
+                            DELETE FROM starboard_data
+                            WHERE star_message_id = $1 AND root_message_id = $2
+                            """, start_board_message.id, payload.message_id)
+                        except (discord.Forbidden, discord.NotFound):
+                            pass
+
+    @commands.Cog.listener()
+    async def on_raw_reaction_remove(self, payload):
+        guild = self.bot.get_guild(payload.guild_id)
+        if guild is not None:
+            if str(payload.emoji) == "\U00002b50":
+                pin = await self.bot.pg_conn.fetchval("""
+                    SELECT star_pin_count 
+                    FROM starboard_config_data 
+                    WHERE guild_id = $1
+                    """, payload.guild_id)
+
+                star_board_channel_id = await self.bot.pg_conn.fetchval("""
+                    SELECT star_channel_id
+                    FROM starboard_config_data
+                    WHERE guild_id = $1
+                    """, payload.guild_id)
+                star_board_channel = self.bot.get_channel(star_board_channel_id)
+                star_channel = self.bot.get_channel(payload.channel_id)
+                if star_board_channel:
+                    star_message_row = await self.bot.pg_conn.fetchrow("""
+                        SELECT * FROM starboard_data 
+                        WHERE root_message_id = $1
+                        """, payload.message_id)
+                    print(star_message_row)
+                    stars = star_message_row[3] if star_message_row else 2
+                    stars -= 1
+                    print(stars)
+                    if stars == pin:
+                        message = await self.bot.get_channel(payload.channel_id).fetch_message(payload.message_id)
+                        embed = discord.Embed(colour=get_colour(), description=message.content)
+                        embed.set_author(name=message.author.name, icon_url=message.author.avatar_url)
+                        embed.timestamp = datetime.datetime.utcnow()
+                        embed.add_field(name="Original", value=f"[Jump Url]({message.jump_url})", inline=False)
+                        msg = await star_board_channel.send(content=f"**{stars}** :star: {star_channel.mention} ID : {payload.message_id}", embed=embed)
+                        await self.bot.pg_conn.execute("""
                             INSERT INTO starboard_data 
                             VALUES ($1, $2, $3, $4)
                             """, payload.guild_id, payload.message_id, msg.id, stars)
-                        elif stars > pin:
-                            try:
-                                start_board_message = await star_board_channel.fetch_message(star_message_row[2])
-                                await start_board_message.edit(content=f"**{stars}** :star: {star_channel.mention} ID : {payload.message_id}")
-                                await self.bot.pg_conn.execute("""
+                    elif stars > pin:
+                        try:
+                            start_board_message = await star_board_channel.fetch_message(star_message_row[2])
+                            await start_board_message.edit(content=f"**{stars}** :star: {star_channel.mention} ID : {payload.message_id}")
+                            await self.bot.pg_conn.execute("""
                                 UPDATE starboard_data 
                                 SET stars = $1 
                                 WHERE star_message_id = $2 AND root_message_id = $3
                                 """, stars, start_board_message.id, payload.message_id)
-                            except (discord.Forbidden, discord.NotFound):
-                                pass
-                        elif stars < pin:
-                            try:
-                                start_board_message = await star_board_channel.fetch_message(star_message_row[2])
-                                await start_board_message.edit(content=f"**{stars}** :star: {star_channel.mention} ID : {payload.message_id}")
-                                await self.bot.pg_conn.execute("""
+                        except (discord.Forbidden, discord.NotFound):
+                            pass
+                    elif stars < pin:
+                        try:
+                            start_board_message = await star_board_channel.fetch_message(star_message_row[2])
+                            await start_board_message.edit(content=f"**{stars}** :star: {star_channel.mention} ID : {payload.message_id}")
+                            await self.bot.pg_conn.execute("""
                                 DELETE FROM starboard_data
                                 WHERE star_message_id = $1 AND root_message_id = $2
                                 """, start_board_message.id, payload.message_id)
-                            except (discord.Forbidden, discord.NotFound):
-                                pass
+                        except (discord.Forbidden, discord.NotFound):
+                            pass
 
 
 def setup(bot):
